79928b86ca852065d69cfc7323f3feb9b902b9cb
Jhanoi Samuels <jhanoi.christina@gmail.com>
2024-10-26 15:00:09 -0500

    Adding an X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,26 @@
+let thisPlayer = 'O';
+const whichPlay = Array(9).fill(null);
+
+
 window.addEventListener('DOMContentLoaded', () => {
     let game = document.getElementById('game');
 
     if (game){
         const squares = game.querySelectorAll('div');
-        squares.forEach(square => {
+        squares.forEach((square, position) => {
             square.classList.add('square');
+            square.addEventListener('click', () => squareClicked(square, position));
         });
     }
 }
 );
+
+function squareClicked(square, position){
+    if (!whichPlay[position]){
+        whichPlay[position] = thisPlayer;
+        square.textContent = thisPlayer;
+        square.classList.add(thisPlayer);
+
+        thisPlayer = thisPlayer === 'O' ? 'x' : 'O';
+    }
+}
